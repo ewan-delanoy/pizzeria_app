77719263a65c5fdf62dc58a4a894e758950f2412
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-28 15:43:48 +0100

    create order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_134425) do
+ActiveRecord::Schema.define(version: 2018_12_28_142103) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
