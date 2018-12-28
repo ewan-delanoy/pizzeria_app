542e41b2a15901e5970ec45f82bee52161db6aa5
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-28 17:30:54 +0100

    add user/order association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,3 @@
 class Order < ApplicationRecord
-
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_145258) do
+ActiveRecord::Schema.define(version: 2018_12_28_151833) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_12_28_145258) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
