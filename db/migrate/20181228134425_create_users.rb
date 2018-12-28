b43de2000fa162db644c720454dcd4fe25d444d6
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-28 15:17:11 +0100

    create user model

---
+++ db/migrate/20181228134425_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_hash
+      t.timestamps
+    end
+  end
+end
