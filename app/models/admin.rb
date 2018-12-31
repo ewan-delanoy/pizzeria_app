20a8fcd58a72e4217a8b3489152529b7b5d5064e
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-31 09:47:48 +0100

    validations for users and admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,6 @@
 class Admin < ApplicationRecord
-
+  has_many :orders
+  validates :name, presence: true,
+  uniqueness: { case_sensitive: false },
+  length: { minimum: 3, maximum: 25 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+  uniqueness: { case_sensitive: false },
+  format: { with: VALID_EMAIL_REGEX }
 end
