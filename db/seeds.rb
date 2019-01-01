c2dc6090de9ebb399a086e593ecf5d73c8a4ae17
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-01 18:38:41 +0100

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email:"luke.skywalker@example.com",
+            first_name:"Luke",
+            last_name:"Skywalker",
+            password_digest:"blank");
+
+User.create(email:"peggy.lee@heythere.com",
+            first_name:"Peggy",
+            last_name:"Lee",
+            password_digest:"blank");
+
+Order.create(recipe:"Calzone",
+            user_id: 2,
+            orderer_first_name:"Peggy",
+            orderer_last_name:"Lee",
+            state:"Reçue");
+
+Order.create(recipe:"Trois fromages",
+            user_id: 2,
+            orderer_first_name:"Luke",
+            orderer_last_name:"Skywalker",
+            state:"Reçue");
+
+
+Order.create(recipe:"Jambon",
+            user_id: 2,
+            orderer_first_name:"Peggy",
+            orderer_last_name:"Lee",
+            state:"Reçue");
+
+Order.create(recipe:"Trois fromages",
+            user_id: 2,
+            orderer_first_name:"Luke",
+            orderer_last_name:"Skywalker",
+            state:"Reçue");
