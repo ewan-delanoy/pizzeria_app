424d85b664c16ea500f9186c034fb7a3504ea725
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-03 15:43:24 +0100

    signup for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,24 @@ class UsersController < ApplicationController
     @users = User.all
   end
   def show
-    @orders = Order.all 
+    @orders = Order.all
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Bienvenue à notre Pizzeria #{@user.first_name} #{@user.last_name}"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#users'
   get '/admin', to: 'welcome#admins'
   resources :orders
-  get '/users/index', to: 'users#index'
-  get '/users/:id', to: 'users#show', as: 'user'
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show,:index,:new,:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
