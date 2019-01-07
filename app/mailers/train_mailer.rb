6fe7d31335053725dd79b0234aab86c5ead4403a
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-07 16:15:06 +0100

    replace welcome_email with send_welcome_email in test code

--- app/mailers/train_mailer.rb
+++ app/mailers/train_mailer.rb
@@ -1,9 +1,9 @@
 class TrainMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def welcome_email
+  def send_welcome_email
     #@user = params[:user]
     #@url  = 'http://example.com/login'
-    mail(to: 'ewan.delanoy@zoho.com', subject: 'This is my first email')
+    mail(to: 'ewan.delanoy@zoho.com', subject: 'This is my first email').deliver_now
   end
 end
