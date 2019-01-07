3819e9e846a5a29d0d3acad693fff6657491638d
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-07 15:49:22 +0100

    shorten localhost name in mail config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'http://localhost:3000',
+  :domain => 'localhost',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
