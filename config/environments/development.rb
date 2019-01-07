4963db7c1c9aa6a135290af345ad63d42daa09a0
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-07 15:39:40 +0100

    replace Google with Sendgrid as a smtp server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,14 +35,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'ewan@capsens.eu',
-    password:             'cKu27P@#capsens2019',
-    authentication:       :login,
-    enable_starttls_auto: true }
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'http://localhost:3000',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
