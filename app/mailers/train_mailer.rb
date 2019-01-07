d84d886b194a1c279fec27a70b8b20d0f2e1733f
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-07 16:56:52 +0100

    change the content of the welcome emails

--- app/mailers/train_mailer.rb
+++ app/mailers/train_mailer.rb
@@ -1,9 +1,9 @@
 class TrainMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'ne-pas-répondre@pizzeria.com'
 
-  def send_welcome_email
+  def welcome_email
     #@user = params[:user]
     #@url  = 'http://example.com/login'
-    mail(to: 'ewan.delanoy@zoho.com', subject: 'This is my first email').deliver_now
+    mail(to: 'ewan.delanoy@zoho.com', subject: 'This is my first email')
   end
 end
