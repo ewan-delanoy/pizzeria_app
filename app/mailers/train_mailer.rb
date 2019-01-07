b436ebad373e410d1a83664038a33fe00db6567b
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-07 11:34:57 +0100

    added content in TrainMailer

--- app/mailers/train_mailer.rb
+++ app/mailers/train_mailer.rb
@@ -1,2 +1,9 @@
 class TrainMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def welcome_email
+    #@user = params[:user]
+    #@url  = 'http://example.com/login'
+    mail(to: 'ewan.delanoy@zoho.com', subject: 'This is my first email')
+  end
 end
