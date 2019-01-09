6ffafb8db62ac3cc48a39d40c5e630fbb45f00db
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-09 17:47:42 +0100

    add index_for_user route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'adminlogin', to: 'adminsessions#new'
   post 'adminlogin', to: 'adminsessions#create'
   delete 'adminlogout', to: 'adminsessions#destroy'
+  get '/users/:user_id/orders', to: 'orders#index_for_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     member do
