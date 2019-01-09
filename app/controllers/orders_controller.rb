b4c88803e170dc15c3ff74cda6d441af749a1174
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-09 17:36:41 +0100

    add index_for_user method in OrderController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,10 @@ class OrdersController < ApplicationController
       redirect_to users_path(current_user)
     end
   end
+  def index_for_user(some_user)
+    @orders = some_user.orders.paginate(page: params[:order_page], per_page: 20)
+    render 'index_for_user'
+  end
   def new
     @order = Order.new
   end
