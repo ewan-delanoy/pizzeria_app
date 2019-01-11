e2b170a04227c2d1d56eaf1dedc49b383b32a140
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-11 16:05:07 +0100

    update users_controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require'rails_helper'
 describe'GET usercontroller#show'do
   it "assigns the requested user to @user" do
       user = create(:user)
-      get :show, id: user
+      get :show, params: { id: user}
       expect(assigns(:user)).to eq user
   end
 end
