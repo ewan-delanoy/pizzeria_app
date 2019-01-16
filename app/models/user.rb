493232848dc7a924e199014cc21a2b8a5aaffcf3
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-16 11:24:43 +0100

    remove superfluous semicolon in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :orders
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
-  validates :mangopay_user_id, presence: true,
-  validates :mangopay_wallet_id, presence: true,
+  validates :mangopay_user_id, presence: true
+  validates :mangopay_wallet_id, presence: true
   uniqueness: { case_sensitive: false },
   format: { with: VALID_EMAIL_REGEX }
   has_secure_password
