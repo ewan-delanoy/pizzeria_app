2fd47f0b8378f2aa37791ee37b9c8b5c5accf3f1
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-16 11:31:20 +0100

    fix syntax error in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :orders
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 105 },
   validates :mangopay_user_id, presence: true
   validates :mangopay_wallet_id, presence: true
-  uniqueness: { case_sensitive: false },
-  format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
   has_secure_password
   def email_activate
     self.email_confirmed = true
